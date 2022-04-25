9a2a69ba092da4fa5d97dacbe0a27981fab97388
Brad Fujizawa <bafujizawa@gmail.com>
2022-04-25 08:33:35 -0700

    finished the code

--- Fujizawa_Brad_Fizzbuzz.js
+++ Fujizawa_Brad_Fizzbuzz.js
@@ -1,6 +1,18 @@
 for(i=1;i<101;i++)
 {
-    if(i % 3 == 0)
+    if(i % 3 == 0 && i % 5 == 0)
+    {
+        console.log('fizzbuzz')
+    }
+    else if(i % 5 ==0)
+    {
+        console.log('buzz')
+    }
+    else if(i % 3 == 0)
+    {
+        console.log('fizz')
+    }
+    else
     {
         console.log(i)
     }
