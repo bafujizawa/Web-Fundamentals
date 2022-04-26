c791e5c98103d133c7258bf3fe10d3a201a6276d
Brad Fujizawa <bafujizawa@gmail.com>
2022-04-25 19:18:36 -0700

    initial commit

---
+++ Fujizawa_Brad_Button_Clicker.js
@@ -0,0 +1,14 @@
+function logout(elem)
+{
+    elem.innerText = "Logout";
+}
+
+function hide(elem)
+{
+    elem.parentElement.remove();
+}
+
+function myFunction()
+{
+    alert("Ninja was liked");
+}
